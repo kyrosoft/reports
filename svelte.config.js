e25847a45d842adaabaa091eceaa6fe63d406915
qyct <rukmangadan.d@gmail.com>
2026-03-11 16:32:39 +0530

    p

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
     adapter: adapter(),
 
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/kyroreports' : ''
+      base: process.env.NODE_ENV === 'development' ? '' : '/kyroreports'
     }
   }
 };
